8530f82c7186430d0581135945e1608eb4813c59
SunshineSummerville <sunshine.summerville@gmail.com>
2021-05-23 15:55:39 +0200

    signup getest. yes hij doet het

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { useForm } from "react-hook-form";
 import './SignUpForm.css'
+import axios from "axios";
 
 export default function SignUpForm() {
     const { register, handleSubmit, errors } = useForm();
 
+    //const userName = useState(")
+
     function onSubmit(data) {
-        //console.log(data)
+        // e.preventDefault()
+        // try {
+        //     const result = await axios.post('http://localhost:8080/api/auth/signup', {register.name,});
+        //
+        //     console.log(result);
+        // } catch (e) {
+        //     console.error(e);
+        //
+        // }
+
+
+        console.log(data)
 
     }
-    //console.log("errors", errors)
+
 
     return <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="userName">Username</label>
@@ -20,11 +34,11 @@ export default function SignUpForm() {
         <input name={"password"} type="password" ref={register({ required: true,minLength:2 })}/>
         {errors.password && <span>This field is required</span>}
 
-        <label htmlFor="firstName">Username</label>
+        <label htmlFor="firstName">Firstname</label>
         <input name={"firstName"} type="text" ref={register({ required: true,minLength:2 })}/>
         {errors.firstName && <span>This field is required</span>}
 
-        <label htmlFor="lastName">Username</label>
+        <label htmlFor="lastName">Lastname</label>
         <input name={"lastName"} type="text" ref={register({ required: true,minLength:2 })}/>
         {errors.lastName && <span>This field is required</span>}
 
@@ -37,23 +51,29 @@ export default function SignUpForm() {
 
         </select>
 
-        <label htmlFor="street">Username</label>
+        <label htmlFor="street">Street</label>
         <input name={"street"} type="text" ref={register({ required: true,minLength:2 })}/>
         {errors.street && <span>This field is required</span>}
 
-        <label htmlFor="houseNumber">Username</label>
+        <label htmlFor="houseNumber">Housenumber</label>
         <input name={"houseNumber"} type="text" ref={register({ required: true,minLength:1 })}/>
         {errors.houseNumber && <span>This field is required</span>}
 
-        <label htmlFor="postalCode">Username</label>
+        <label htmlFor="postalCode">Postalcode</label>
         <input name={"postalCode"} type="text" ref={register({ required: true,minLength:4 })}/>
         {errors.postalCode && <span>This field is required</span>}
 
-        <label htmlFor="email">Username</label>
+        <label htmlFor="email">Email</label>
         <input name={"email"} type="text" ref={register({ required: true,minLength:2, pattern:/^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/ })}/>
         {errors.email && <span>This field is required</span>}
 
-        <input type="submit" ref={register}/>
+        <input type="submit"
+
+               />
+
+
     </form>
 
 }
+
+// bij r 56 submit nog een //onClick={onSubmit} toevoegen?
