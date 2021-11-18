4171e8237f9cd49ca17320e448b4d7f1b616cd06
SunshineSummerville <sunshine.summerville@gmail.com>
2021-11-18 14:02:18 +0100

    Removed the logs in MyServices

--- src/pages/MyServices.js
+++ src/pages/MyServices.js
@@ -2,33 +2,29 @@ import React, {useEffect, useState, useContext} from 'react'
 import axios from "axios";
 import {AuthContext} from "../context/AuthContext";
 
-export default function MyServices () {
+export default function MyServices() {
     const [allCategories, setAllcategories] = useState([]);
-    const {authState} = useContext (AuthContext);
-    console.log("AUTH IN MY SERVICES",authState)
+    const {authState} = useContext(AuthContext);
 
 
-
-    useEffect (()=> {
-        // console.log("USEEFFECT WERKT?",useEffect)
-        async  function getAllCategories () {
-            const result = await axios.get ("http://localhost:8080/api/categories");
-        // console.log(result.data)
+    useEffect(() => {
+        async function getAllCategories() {
+            const result = await axios.get("http://localhost:8080/api/categories");
             setAllcategories(result.data)
         }
+
         getAllCategories()
 
     }, [])
 
 
-
     const handymanId = authState.user.id
-    async function updateService (categoryId,httpMethod) {
+
+    async function updateService(categoryId, httpMethod) {
         const requestbody = {
             categoryId: categoryId
         }
-        console.log("Hello World", categoryId,handymanId, authState.token)
-        if (httpMethod=== "post"){
+        if (httpMethod === "post") {
             const response = await axios.post(`http://localhost:8080/api/user/${handymanId}/categories`, requestbody, {
                 headers: {
                     "Content-Type": "application/json",
@@ -36,8 +32,7 @@ export default function MyServices () {
                 }
             })
 
-            console.log("RESPONSE",response)
-        }else if(httpMethod=== "delete"){
+        } else if (httpMethod === "delete") {
             const response = await axios.delete(`http://localhost:8080/api/user/${handymanId}/categories`, {
                 headers: {
                     "Content-Type": "application/json",
@@ -47,13 +42,11 @@ export default function MyServices () {
 
             })
 
-            console.log("RESPONSE",response)
 
         }
 
-        const result = await axios.get ("http://localhost:8080/api/categories");
+        const result = await axios.get("http://localhost:8080/api/categories");
         setAllcategories(result.data)
-
 
 
     }
@@ -61,16 +54,15 @@ export default function MyServices () {
 
     return (
         <div>
-            {allCategories.map((category)=> {
-                // console.log("Category",category)
-                const isHandymanForThisCategory = category.handymen.some((handyman)=> {
-                    // console.log("HANDYMAN",handyman.id, handymanId, handyman.id === handymanId)
+            {allCategories.map((category) => {
+                const isHandymanForThisCategory = category.handymen.some((handyman) => {
                     return handyman.id === handymanId
                 })
-                // console.log("CHECK",isHandymanForThisCategory)
 
                 return <div key={category.id}>
-                        <input type="checkbox" id="" name="" onChange={(event)=>updateService(category.id,event.target.checked?"post":"delete")} checked={isHandymanForThisCategory}/>
+                    <input type="checkbox" id="" name=""
+                           onChange={(event) => updateService(category.id, event.target.checked ? "post" : "delete")}
+                           checked={isHandymanForThisCategory}/>
 
                     {category.name} - {category.jobDescription}
                 </div>
@@ -80,8 +72,7 @@ export default function MyServices () {
 
         </div>
 
-        );
-
+    );
 
 
 }
