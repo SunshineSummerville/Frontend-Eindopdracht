f9ac53e2729fdac891a0fcc5a30aa36a92cec158
SunshineSummerville <sunshine.summerville@gmail.com>
2021-11-25 13:04:26 +0100

    Implement edit for Reservationdate

--- src/pages/ReservationDetails.js
+++ src/pages/ReservationDetails.js
@@ -3,28 +3,17 @@ import {Link, useParams} from "react-router-dom";
 import axios from "axios";
 import {AuthContext} from "../context/AuthContext";
 
-// [] replace hello world with reservationpage component
-// [] GET reservation Id from the url using use Params from react router
-// []Make a get request to the backend api/reservation/24
 
-export default function ReservationDetails(){
+export default function ReservationDetails() {
     const params = useParams();
     const [reservationDetails, setReservationDetails] = useState();
-    const {authState} = useContext (AuthContext);
-    const [editMode,setEditmode] = useState(false);
-    const [newDate,setNewdate] = useState();
-    console.log("EDITMODE", editMode)
-    console.log("New Date",newDate)
+    const {authState} = useContext(AuthContext);
+    const [editMode, setEditmode] = useState(false);
+    const [newDate, setNewdate] = useState();
 
-    console.log("PARAMS do we see the reservation ID?", params)
-    console.log("AUTH IN RESERVATION DETAILS", authState)
-
-    useEffect(()=> {
+    useEffect(() => {
         async function getReservationDetails() {
-            // const reservationId = {
-            //     reservationId:reservationId
-            // }
-            const result = await axios.get(`http://localhost:8080/api/reservation/${params.reservationId}`,{
+            const result = await axios.get(`http://localhost:8080/api/reservation/${params.reservationId}`, {
                 headers: {
                     "Content-Type": "application/json",
                     Authorization: `Bearer ${authState.token}`,
@@ -32,37 +21,30 @@ export default function ReservationDetails(){
             })
             setReservationDetails(result.data);
             setNewdate(result.data.reservationDate);
-            console.log("Result what is the result of our request",result);
         }
 
         getReservationDetails()
-    },[])
+    }, [])
 
-    console.log("DETAILS", reservationDetails)
-    console.log("ID", reservationDetails && reservationDetails.id)
 
-    const changeEditmode = ()=> {
+    const changeEditmode = () => {
         setEditmode(true)
-        // console.log("Hello world")
     }
 
-    const changeTheDate = (event)=> {
+    const changeTheDate = (event) => {
         setNewdate(event.target.value);
-        console.log("Hello world",event.target.value)
     }
 
-    const today= new Date()
-    console.log(today.getDate())
-    const minDate= `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`
+    const today = new Date()
+    const minDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
 
-    const saveTheDate = ()=> {
-        console.log("SAVE THE DATE",newDate, reservationDetails.id, authState.token)
+    const saveTheDate = () => {
         modifyReservation();
     }
 
-    async  function modifyReservation() {
+    async function modifyReservation() {
         const requestbody = {
-            reservationDate:newDate
+            reservationDate: newDate
         }
         const response = await axios.patch(`http://localhost:8080/api/reservation/${params.reservationId}`, requestbody, {
             headers: {
@@ -71,29 +53,29 @@ export default function ReservationDetails(){
             }
         })
 
-        console.log("PATCH RESPONSE",response)
+        setReservationDetails(response.data)
+        setEditmode(false)
 
     }
 
 
-
     return (
-        <div className= 'reservation details'>
+        <div className='reservation details'>
             <h2>Reservation Details</h2>
-            {reservationDetails && <img src={reservationDetails.image} alt="Your uploaded file" />}
-            <p>Reserveringsnummer: { reservationDetails && reservationDetails.id}</p>
-            {editMode ? <input onChange={changeTheDate} type="date" min={minDate} value={newDate}/> : <p>Datum: { reservationDetails && reservationDetails.reservationDate}</p>}
-            <p>Category: { reservationDetails && reservationDetails.category.name}</p>
+            {reservationDetails && <img src={reservationDetails.image} alt="Your uploaded file"/>}
+            <p>Reserveringsnummer: {reservationDetails && reservationDetails.id}</p>
+            {editMode ? <input onChange={changeTheDate} type="date" min={minDate} value={newDate}/> :
+                <p>Datum: {reservationDetails && reservationDetails.reservationDate}</p>}
+            <p>Category: {reservationDetails && reservationDetails.category.name}</p>
             <p>Contact gegevens: </p>
-            <p>Handyman: { reservationDetails && reservationDetails.handyman.firstname} - { reservationDetails && reservationDetails.handyman.lastname}</p>
-            <p>Email: { reservationDetails && reservationDetails.handyman.email}</p>
-            <p>Telefoonnummer: { reservationDetails && reservationDetails.handyman.phonenumber}</p>
+            <p>Handyman: {reservationDetails && reservationDetails.handyman.firstname} - {reservationDetails && reservationDetails.handyman.lastname}</p>
+            <p>Email: {reservationDetails && reservationDetails.handyman.email}</p>
+            <p>Telefoonnummer: {reservationDetails && reservationDetails.handyman.phonenumber}</p>
 
-            {editMode ? <button onClick={saveTheDate}>Save</button> :  <button onClick={changeEditmode}>edit</button>}
+            {editMode ? <button onClick={saveTheDate}>Save</button> : <button onClick={changeEditmode}>edit</button>}
 
 
-
-                </div>
+        </div>
     )
 
 
